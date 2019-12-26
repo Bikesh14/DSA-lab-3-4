ac5644b7b11b6fd9ac962bbca684329677628aa9
Bikesh <bimalibikesh@gmail.com>
2019-12-26 10:21:51 +0545

    abstract class defined

---
+++ binarytree.h
@@ -0,0 +1,11 @@
+#ifndef binarytree_h
+#define binarytree_h
+
+ class BinarySearchTree{
+	private:
+	public:
+		virtual void preordertraversal()=0;
+		virtual void add(int data)=0;
+		void bool search(int data)=0;
+};
+#endif
