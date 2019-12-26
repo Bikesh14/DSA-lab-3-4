7e91fed13298c56e727bde212b0856332536f017
ShrawakB <55523949+ShrawakB@users.noreply.github.com>
2019-12-26 10:02:14 +0545

    array wala haii

---
+++ ArrayBst.cpp
@@ -0,0 +1,14 @@
+# ifndef 
+# include<iostream>
+using namespace std;
+
+class ArrayBST : public BinarySearchTree
+{
+private: 
+	int elemets[16];
+public:
+	void preorderTraversal();
+	void add(int data);
+	bool search (int date);
+			
+}
