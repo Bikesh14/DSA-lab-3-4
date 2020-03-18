bfa075b254fa76e555b858d2c2672c44f75d214c
ShrawakB <55523949+ShrawakB@users.noreply.github.com>
2020-03-18 22:33:40 +0545

    Update binarytree.h

--- binarytree.h
+++ binarytree.h
@@ -2,7 +2,7 @@
 #define BINARYTREE_h
 class BinarySearchTree{
 		public:
-			virtual void preorderTraversal()=0;
+			virtual void preorder(int x)=0;
 			virtual void add(int data)=0;
 			virtual bool search(int data)=0;
 
