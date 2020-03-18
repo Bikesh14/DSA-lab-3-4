20a9f223611143f4a77f2b3f4167da76796ccdbd
ShrawakB <55523949+ShrawakB@users.noreply.github.com>
2020-03-18 22:14:58 +0545

    Update Arraymain.cpp

--- Arraymain.cpp
+++ Arraymain.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 int main(){
 	ArrayBST a;
-	a.add(20);
-	a.add(27);
+	a.add(15);
+	a.add(10);
 	a.add(11);
 	a.add(14);
-	a.add(44);
-	a.add(49);
+	a.add(13);
+	a.add(4);
 	a.add(1);
-	a.add(88);
-	a.add(19);
+	a.add(5);
+	a.add(16);
 	a.add(18);
-	a.add(14);
-	a.add(60);
+	a.add(17);
+	a.add(20);
 	for(int i=0;i<MAX_SIZE;i++){
 		cout<<a.elements[i]<<",";
 	}
 	cout<<endl;
 	a.preorder(1);
 	cout<<endl;
-	a.search(60);
+	a.search(33);
 	a.search(29);
 	int b;
 	b=a.max();
@@ -31,6 +31,4 @@ int main(){
 	a.inorder(1);
 	return 0;
 }
-
-
 
